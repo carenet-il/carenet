c1d3f0270fc1321e0e466f15f4934c58bbd2afe0
liranzxc <lirannh@gmail.com>
2024-01-05 15:47:01 +0200

    implement pinecone

--- apps/crawler-feeds/src/app.py
+++ apps/crawler-feeds/src/app.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 from libs.embedding.quora_distilbert_multilingual_embedding import QuoraDistilBertMultilingualEmbedding
@@ -9,7 +10,10 @@ from libs.vector_storage.vector_storage import VectorStorage
 
 def main():
     embedding_model = QuoraDistilBertMultilingualEmbedding()
-    storage_provider = PineconeVectorProvider(embedding_model=embedding_model)
+
+    storage_provider = PineconeVectorProvider(embedding_model=embedding_model, api_key=os.getenv("PINECONE_API_KEY"),
+                                              environment=os.getenv("PINECONE_ENVIRONMENT"),
+                                              index_name=os.getenv("PINECONE_INDEX_NAME"))
 
     vector_storage = VectorStorage(storage_provider=storage_provider)
     n12_feed = N12Feed()
