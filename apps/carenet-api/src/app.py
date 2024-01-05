cadda52800b5cf8a5ee50652f154e9c5a68bf33e
liranzxc <lirannh@gmail.com>
2024-01-05 15:29:03 +0200

    add baseline of classes

--- apps/carenet-api/src/app.py
+++ apps/carenet-api/src/app.py
@@ -1,21 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/document/insertMany', methods=['POST'])
 def insert_many():
-    if request.json:
-        return jsonify({"message": "Data inserted successfully!"}), 200
-    else:
-        return jsonify({"error": "No data provided"}), 400
+    return {"ok": 200}
 
 
 @app.route('/document/search', methods=['POST'])
 def search():
-    query = request.args.get('query')
-    if query:
-        # Simple search implementation (case-insensitive)
-        return jsonify({"ok": 200}), 200
-    else:
-        return jsonify({"error": "No query provided"}), 400
+    return {"ok": 200}
--- apps/crawler-feeds/src/app.py
+++ apps/crawler-feeds/src/app.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from libs.embedding.minilmv6_embedding import MiniLMv6Embedding
+from libs.embedding.quora_distilbert_multilingual_embedding import QuoraDistilBertMultilingualEmbedding
 from libs.feed.n12_feed import N12Feed
 from libs.interfaces.document import Document
 from libs.vector_storage.vector_provider.pincone_vector_provider import PineconeVectorProvider
@@ -8,7 +8,7 @@ from libs.vector_storage.vector_storage import VectorStorage
 
 
 def main():
-    embedding_model = MiniLMv6Embedding()
+    embedding_model = QuoraDistilBertMultilingualEmbedding()
     storage_provider = PineconeVectorProvider(embedding_model=embedding_model)
 
     vector_storage = VectorStorage(storage_provider=storage_provider)
