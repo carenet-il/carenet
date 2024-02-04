49db19cb2f81df331dbc2b4a0dd902e6ce786f35
liranzxc <lirannh@gmail.com>
2024-02-04 11:42:16 +0200

    deploy ftp

--- apps/carenet-frontend/next.config.mjs
+++ apps/carenet-frontend/next.config.mjs
@@ -12,8 +12,8 @@ const nextConfig = {
   // if using docker uncomment this
   // output: 'standalone'
   // for git pages use the two lines below
-  output: "export",
-  basePath: "/carenet",
+//   output: "export",
+//   basePath: "/carenet",
 };
 
 export default nextConfig;
