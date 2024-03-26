10482d37d59d3bf8d640332649ccf620d0f6682e
Omer Cohen <omercotkd@gmail.com>
2024-03-26 20:54:50 +0200

    added favicon

--- apps/carenet-frontend/src/app/layout.tsx
+++ apps/carenet-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="he">
+      <Head>
+        <link
+          rel="/favicon_io/apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="/favicon_io/icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="/favicon_io/icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="/favicon_io/manifest" href="/site.webmanifest" />
+      </Head>
       <GoogleAnalytics gaId="G-28BG9RD7VT" />
       <body className={inter.className} dir={"rtl"}>
         {children}
